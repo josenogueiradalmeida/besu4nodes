541ea88a787f8300245042d60f5c05b534c20bf5
José Nogueira <josej@bdnes.gov.br>
2020-06-01 18:12:32 -0300

    first

---
+++ startNodes.bat
@@ -0,0 +1,18 @@
+for /l %%x in (1, 1, 4) do  (
+    echo %%x
+    cd node%%x
+    cd data
+    del /Q /S besu.networks
+    del /Q /S besu.ports
+    del /Q /S caches
+    del /Q /S database
+    del /Q /S DATABASE_METADATA.json
+    del /Q /S uploads
+    cd ..
+    start besu --config-file=configNode.toml
+    cd ..
+    pause
+)
+echo.
+echo INICIADOS OS QUATRO NODES
+echo. 
