673801b68bfdc42a1376cf047c52c60784acd867
José Nogueira <josej@bdnes.gov.br>
2020-06-17 11:59:22 -0300

    New local node script

---
+++ runLocalNode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#RUN ONE SPECIFIED NODE
+echo NODE $1
+cd node$1
+cd data
+sudo rm -rf besu.networks
+sudo rm -rf besu.ports
+sudo rm -rf caches
+sudo rm -rf database
+sudo rm -rf DATABASE_METADATA.json
+sudo rm -rf uploads
+cd ..
+sudo docker run  --network=host -v /home/josej/meu/git/besu4nodes/node$1/:/conf/local -v /home/josej/meu/git/besu4nodes/node$1/data:/conf/data rbb_bootnode:latest
